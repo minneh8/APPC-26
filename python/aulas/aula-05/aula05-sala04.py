774c8c93ee1b8306d7b5681910871d3acd4eda6a
Lucas <lucasminneh270508@gmail.com>
2026-03-23 12:44:45 -0300

    Create aula05-sala04.py

---
+++ python/aulas/aula-05/aula05-sala04.py
@@ -0,0 +1,6 @@
+nome = "juvenal"
+pos = 0
+while(pos <= 6):
+    if ((nome[pos] == 'a') or (nome[pos] == 'e') or (nome[pos] == 'i') or (nome[pos] == 'o') or (nome[pos] == 'u')):
+        print("pos = ", pos, "letra =  ", nome[pos])
+    pos = pos + 1
