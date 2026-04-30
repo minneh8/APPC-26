db9ad473009120b599e92eda7d066b4954a7d21d
Lucas <lucasminneh270508@gmail.com>
2026-04-30 16:45:00 -0300

    Finalizada a tarefa de crud!

--- python/tarefas/tarefa-crud.py
+++ python/tarefas/tarefa-crud.py
@@ -11,14 +11,19 @@
 #5- Consultar
 #0- Sair
 
-dados= []
+dados=[ 
+       ["ADAMASTHOR","M","19 912345678","adm@gmail.com",18],
+       ["MARISMENIA","F","19 956776564","mari@gmail.com",76],
+       ["ONLAYNY","F","19 934786523","onlayny@gmail.com",22],
+       ["OFFYLAYNY","F","19 945123278","offylayny@gmail.com",35],
+       ["HORKUTYZ","M","19 990091237","hkt@gmail.com",27]
+       ]
 opcao = 0
 continuar = "S"
 print("Bem Vindo ao Software Timão - Agenda de Contatos\n")
 while True:
     print("Escolha a opção desejada: \n1- Cadastrar \n2- Listar \n3- Alterar \n4- Deletar \n5- Consultar \n0- Sair\n")
     opcao = int(input("Digite a opção desejada: "))
-
     match opcao:
         case 1:
             print("-----------/ Opção Cadastrar /----------")
@@ -48,8 +53,54 @@ while True:
                 pos += 1
         case 3:
             print("-----------/ Opção Alterar /----------")
+            if len(dados) == 0:
+                print("Não há contatos cadastrados para alterar\n")
+                continue
+            nomealtera = input("Digite o nome do contato que deseja alterar: ")
+            pos = 0
+            encontrado = False
+            while pos < len(dados):
+                if nomealtera == dados[pos][0]:
+                    print(f"Contato encontrado: {dados[pos]}")
+                    print("Digite os novos dados (Enter para manter o atual):")
+                    novo_nome = input(f"Nome [{dados[pos][0]}]: ")
+                    novo_sexo = input(f"Sexo [{dados[pos][1]}]: ")
+                    novo_celular = input(f"Celular [{dados[pos][2]}]: ")
+                    novo_email = input(f"Email [{dados[pos][3]}]: ")
+                    novo_idade = input(f"Idade [{dados[pos][4]}]: ")
+                    if novo_nome: dados[pos][0] = novo_nome
+                    if novo_sexo: dados[pos][1] = novo_sexo
+                    if novo_celular: dados[pos][2] = novo_celular
+                    if novo_email: dados[pos][3] = novo_email
+                    if novo_idade: dados[pos][4] = novo_idade
+                    print("Contato alterado com sucesso!\n")
+                    encontrado = True
+                    break
+                pos += 1
+            if not encontrado:
+                print("Contato não encontrado\n")
         case 4:
             print("-----------/ Opção Deletar /----------")
+            if len(dados) == 0:
+                print("Não há contatos cadastrados para deletar\n")
+                continue
+            nomedel = input("Digite o nome do contato que deseja deletar: ")
+            pos = 0
+            encontrado = False
+            while pos < len(dados):
+                if nomedel == dados[pos][0]:
+                    print(f"Contato encontrado: {dados[pos]}")
+                    continuar = input("Tem certeza que deseja deletar? (S/N): ")
+                    if continuar == "S" or continuar == "s":
+                        dados.pop(pos)
+                        print("Contato deletado com sucesso!\n")
+                    else:
+                        print("Operação cancelada\n")
+                    encontrado = True
+                    break
+                pos += 1
+            if not encontrado:
+                print("Contato não encontrado\n")
         case 5:
             print("-----------/ Opção Consultar /----------")
             if len(dados) == 0:
